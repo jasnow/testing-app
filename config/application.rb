a0caad0d13ddd8ac19b4a3f754da21486250025e
Al Snow <jasnow@hotmail.com>
2023-10-15 10:14:39 -0400

    Upgraded gem(s): [actioncable,actionmailbox,actionmailer,actionpack,actiontext,actionview,activejob,activemodel,activerecord,activestorage,activesupport,base64,bigdecimal,config.active_record.legacy_connection_handling,connection_pool,drb,erubi,io-console,irb,method_source,mini_mime,mutex_m,nokogiri,rack,rack-session,rackup,rails,rails-dom-testing,rails-html-sanitizer,railties,rdoc,reline,ruby2_keywords,thor,timeout,webrick,zeitwerk]

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,5 @@ module TestingApp
     config.generators.javascript_engine = :js
 
     config.action_dispatch.return_only_media_type_on_content_type = false
-
-    config.active_record.legacy_connection_handling = false
   end
 end
